6934304a6d756acb99b6ae5d319e9c1b5eeb6673
Wei-Chen Ling <riddle.apple@gmail.com>
2012-08-06 11:37:02 +0800

    code cleanup

--- lib/application.rb
+++ lib/application.rb
@@ -1,42 +1,33 @@
 require 'rubygems' # disable this for a deployed application
 require 'hotcocoa'
 
+
+
 class WretchDL
-  include HotCocoa
+    include HotCocoa
 
-  def start
-    application name: 'WretchDL' do |app|
-      app.delegate = self
-      window frame: [100, 100, 500, 500], title: 'WretchDL' do |win|
-        win << label(text: 'Hello from HotCocoa', layout: {start: false})
-        win.will_close { exit }
-      end
+    def start
+        application name: 'WretchDL' do |app|
+            app.delegate = self
+            window(size: [600, 500], center: true, title: 'WretchDL') do |win|
+                win.will_close { exit }
+                
+            end
+        end
     end
-  end
 
-  # file/open
-  def on_open(menu)
-  end
 
-  # file/new
-  def on_new(menu)
-  end
+    # help menu item
+    def on_help(menu)
+    end
 
-  # help menu item
-  def on_help(menu)
-  end
+    # This is commented out, so the minimize menu item is disabled
+    #def on_minimize(menu)
+    #end
 
-  # This is commented out, so the minimize menu item is disabled
-  #def on_minimize(menu)
-  #end
-
-  # window/zoom
-  def on_zoom(menu)
-  end
-
-  # window/bring_all_to_front
-  def on_bring_all_to_front(menu)
-  end
+    # window/zoom
+    def on_zoom(menu)
+    end
 end
 
 WretchDL.new.start
